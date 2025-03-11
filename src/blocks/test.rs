2653539e8ec65ec1afed8b502173403148fbc446
zxfae33 <lecro.philippe@icloud.com>
2025-03-11 22:47:11 +0100

    others block tests

--- src/blocks/test.rs
+++ src/blocks/test.rs
@@ -1,5 +1,3 @@
-use sha2::digest::typenum::assert_type;
-
 use super::Block;
 
 //@Notice : Block creation; datas correctly set; hash_calculation(assert_ne!)
@@ -25,4 +23,35 @@ fn genesis_block() {
     assert_eq!(genesis_block.prev_hash, [0u8; 32]);
 
     assert_ne!(genesis_block.hash, [0u8; 32])
+}
+
+//@Notice : Compare same hashblock for test hash_calculate function
+//@Dev : with other data; hash must be != equal
+#[test]
+fn test_hash() {
+    let data = vec!["Hello World !".to_string()];
+    let prev_hash = [2u8; 32];
+    let block1 = Block::new(&data, &prev_hash);
+    // Will be same
+    let hash_calculated = block1.calculate_hash();
+    assert_eq!(block1.hash, hash_calculated);
+
+    let other_data = vec!["Others datas !".to_string()];
+    let block2 = Block::new(&other_data, &prev_hash);
+
+    println!("Bloc 1 hash: {}i\n", hex::encode(block1.hash));
+    println!("Bloc 2 hash: {}", hex::encode(block2.hash));
+
+    assert_ne!(block1.hash, block2.hash);
+}
+
+#[test]
+fn test_blockchain_block() {
+    let genesis = Block::genesis_block();
+    let next_data = vec!["Next Block".to_string()];
+
+    let next_block = Block::new(&next_data, &genesis.hash);
+    //@Dev : nextblock.prev must be equal to genesis hash
+    assert_eq!(next_block.prev_hash, genesis.hash);
+    assert_ne!(genesis.hash, next_block.hash);
 }
